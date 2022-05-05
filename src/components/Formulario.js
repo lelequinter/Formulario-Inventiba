dc4ecb186c2df06ff1efb4f3d131247f7749de1d
Leyder Quintero <lele.quinter@gmail.com>
2022-05-04 22:13:50 -0500

    Añadiendo iconos y reparando dependencias Formik, styledComponents.

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import { FaChevronRight } from "react-icons/fa";
 import { Container, StyledTitle, DivForm, Button } from "../styles/styles";
 import "../index.css";
 
@@ -68,19 +69,7 @@ export const Formulario = () => {
               />
               <Button>
                 <p>Siguiente</p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-chevron-right"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                  />
-                </svg>
+                <FaChevronRight/>
               </Button>
             </DivForm>
           </Form>
