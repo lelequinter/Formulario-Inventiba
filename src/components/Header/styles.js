d986d903f5337da08bf1970e506b2968a8ae99bb
Leyder Quintero <lele.quinter@gmail.com>
2022-05-12 10:40:02 -0500

    Responsive en el header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,19 +8,36 @@ export const Container = styled.div`
   display: flex;
   height: 170px;
   justify-content: center;
+
+  @media (max-width: 400px) {
+    height: 130px;
+  }
 `;
 
 export const StyledLogo = styled.div`
   background: url(${LogoImg});
-  height: 185px;
+  background-size: cover;
+  height: 190px;
   transform: scale(0.5);
   width: 999px;
+
+  @media (max-width: 1000px) {
+    height: 70px;
+    transform: scale(1);
+    width: 370px;
+  }
+
+  @media (max-width: 400px) {
+    height: 64px;
+    transform: scale(1);
+    width: 338px;
+  }
 `;
 
 export const StyledDivider = styled.div`
   background: #063954;
   border-top: 1.5px solid;
-  border-color: #9CCA1F;
+  border-color: #9cca1f;
   height: 50px;
   width: 100%;
 `;
